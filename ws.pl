621e215aef2fce571f43807cf73462496eb10a43
lancegin <gin.lance.inside@hotmail.com>
2017-05-31 19:37:21 +0800

    upd: show live weather

--- ws.pl
+++ ws.pl
@@ -10,9 +10,9 @@ $GD_KEY = "c1f8aa28560b5144e73a80205fae124b";
 sub get_weather {
     my $ua = LWP::UserAgent->new;
     my $url = "http://restapi.amap.com/v3/weather/weatherInfo";
-    my $city = "武汉";
-    my $extention = "base";
-    my $args = "?key=$GD_KEY\&city=$city\&extention=$extention";
+    my $city = $_[0];
+    my $extensions = $_[1];
+    my $args = "?key=$GD_KEY\&city=$city\&extensions=$extensions";
     my $req = HTTP::Request->new(GET => $url.$args);
 
     my $res = $ua->request($req);
@@ -25,27 +25,34 @@ sub get_weather {
     }
 }
 
-sub weather_forecast {
-    my $city = $_[0];
-    say "this is the forecast weather of $city."
+sub show_live {
+    my $data = $_[0];
+    my $live = $data->{"lives"}->[0];
+    say "您好！您查询的".$live->{"city"}."实时天气情况如下：";
+    say "   位置：".$live->{"city"};
+    say "   天气：".$live->{"weather"};
+    say "   气温：".$live->{"temperature"};
+    say "   空气湿度：".$live->{"humidity"};
+    say "   风向：".$live->{"winddirection"};
+    say "   风力：".$live->{"windpower"};
 }
 
-sub weather_live {
-    my $city = $_[0];
-    say "this is the live weather of $city."
+sub show_forecast {
+    my $data = $_[0];
+    say $data;
 }
 
-sub main {
-    my %opt;
-    getopts('a:', \%opt);
-    if ($opt{a}) {
-        weather_forecast($opt{a});
-    } else {
-        my $weather = get_weather;
-        my $json_scalar = from_json($weather);
-        say $json_scalar->{"lives"}->[0]->{"weather"};
-    }
+my %opt;
+my $weather;
+my $json_scalar;
+
+getopts('a:', \%opt);
+if ($opt{a}) {
+    $weather = get_weather($opt{a}, "all");
+    $json_scalar = from_json($weather);
+    show_forecast($json_scalar);
+} else {
+    $weather = get_weather($ARGV[0], "base");
+    $json_scalar = from_json($weather);
+    show_live($json_scalar);
 }
-
-main;
-
